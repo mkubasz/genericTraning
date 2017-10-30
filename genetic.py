a86e276e537a154f0b75e47d106664e926f4ff6e
kmateusz <mateusz.kubaszek@nokia.com>
2017-10-30 16:32:43 +0100

    Create unit tests

--- genetic.py
+++ genetic.py
@@ -1,38 +1,40 @@
 import random
 
-def _generate_parent(length, geneSet):
+
+def _generate_parent(length, gene_set):
     genes = []
     while len(genes) < length:
-        sampleSize = min(length - len(genes), len(geneSet))
-        genes.extend(random.sample(geneSet, sampleSize))
+        sample_size = min(length - len(genes), len(gene_set))
+        genes.extend(random.sample(gene_set, sample_size))
     return ''.join(genes)
 
 
-def _mutate(parent, geneSet):
+def _mutate(parent, gene_set):
     index = random.randrange(0, len(parent))
-    childGenes = list(parent)
-    newGene, alternate = random.sample(geneSet, 2)
-    childGenes[index] = alternate if newGene == childGenes[index] else newGene
-    return ''.join(childGenes)
+    child_genes = list(parent)
+    new_gene, alternate = random.sample(gene_set, 2)
+    child_genes[index] = alternate if new_gene == child_genes[index] else new_gene
+    return ''.join(child_genes)
 
 
-def get_best(get_fitness, targetLen, optimalFitness, geneSet, display):
+def get_best(get_fitness, target_len, optimal_fitness, gene_set, display):
     random.seed()
-    bestParent = _generate_parent(targetLen, geneSet)
-    bestFitness = get_fitness(bestParent)
-    display(bestParent)
-    if bestFitness >= optimalFitness:
-        return bestParent
+    best_parent = _generate_parent(target_len, gene_set)
+    best_fitness = get_fitness(best_parent)
+    display(best_parent)
+
+    if best_fitness >= optimal_fitness:
+        return best_parent
 
     while True:
-        child = _mutate(bestParent, geneSet)
-        childFitness = get_fitness(child)
 
-        if bestFitness >= childFitness:
+        child = _mutate(best_parent, gene_set)
+        child_fitness = get_fitness(child)
+
+        if best_fitness >= child_fitness:
             continue
         display(child)
-        if childFitness >= optimalFitness:
-            break
-        bestFitness = childFitness
-        bestParent = child
-
+        if child_fitness >= optimal_fitness:
+            return child
+        best_fitness = child_fitness
+        best_parent = child
--- guessPassword.py
+++ guessPassword.py
@@ -1,17 +1,17 @@
 import datetime
 import genetic
+import unittest
 from string import ascii_letters
 
 
-def display(genes, target, startTime):
-    timeDiff = datetime.datetime.now() - startTime
+def display(genes, target, start_time):
+    time_diff = datetime.datetime.now() - start_time
     fitness = get_fitness(genes, target)
-    print("{0}\t{1}\t{2}".format(genes, fitness, str(timeDiff)))
+    print("{0}\t{1}\t{2}".format(genes, fitness, str(time_diff)))
 
 
 def get_fitness(genes, target):
     return sum(1 for expected, actual in zip(target, genes) if expected == actual)
-
 
 
 def test_Hello_World():
@@ -21,19 +21,42 @@ def test_Hello_World():
 
 def guess_password(target):
     genset = ascii_letters + ' .!'
-    startTime = datetime.datetime.now()
+    start_time = datetime.datetime.now()
 
     def fnGetFitness(genes):
         return get_fitness(genes, target)
-    
+
     def fnDisplay(genes):
-        return display(genes, target, startTime)
+        return display(genes, target, start_time)
 
     optimalFitness = len(target)
-    genetic.get_best(fnGetFitness, 
-            len(target),
-            optimalFitness,
-            genset, 
-            fnDisplay)
+    genetic.get_best(fnGetFitness,
+                     len(target),
+                     optimalFitness,
+                     genset,
+                     fnDisplay)
+
+
+class GuessPasswordTests(unittest.TestCase):
+    def test_Hello_World(self):
+        target = "Hello World!"
+        self.guess_password(target)
+
+    def guess_password(self, target):
+        geneset = ascii_letters + ' .!'
+        start_time = datetime.datetime.now()
+
+        def fnGetFitness(genes):
+            return get_fitness(genes, target)
+
+        def fnDisplay(genes):
+            return display(genes, target, start_time)
+
+        optimal_fitness = len(target)
+        best = genetic.get_best(fnGetFitness, len(target), optimal_fitness,
+                                geneset, fnDisplay)
+        self.assertEqual(best, target)
+
+
 if __name__ == '__main__':
-    test_Hello_World()
+    unittest.main()
